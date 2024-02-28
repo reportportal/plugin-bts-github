c969243c9a8883546f290dcad2b67e8858721dd3
pigorv <igor.prisyazhniy@gmail.com>
2024-02-28 16:55:33 +0200

    Adds Copyright

--- plugin/src/main/java/com/epam/reportportal/extension/github/command/RetrieveCreateParamsCommand.java
+++ plugin/src/main/java/com/epam/reportportal/extension/github/command/RetrieveCreateParamsCommand.java
@@ -1,3 +1,20 @@
+/*
+ * Copyright 2023 EPAM Systems
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
 package com.epam.reportportal.extension.github.command;
 
 import com.epam.reportportal.extension.CommonPluginCommand;
@@ -14,6 +31,9 @@ import static com.epam.reportportal.extension.github.command.GitHubProperty.URL;
 import static com.epam.ta.reportportal.commons.validation.BusinessRule.expect;
 
 
+/**
+ * @author RiverSharks
+ */
 @RequiredArgsConstructor
 public class RetrieveCreateParamsCommand implements CommonPluginCommand<Map<String, Object>> {
 
--- plugin/src/main/java/com/epam/reportportal/extension/github/info/impl/PropertiesFilePluginInfoProvider.java
+++ plugin/src/main/java/com/epam/reportportal/extension/github/info/impl/PropertiesFilePluginInfoProvider.java
@@ -1,3 +1,20 @@
+/*
+ * Copyright 2023 EPAM Systems
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
 package com.epam.reportportal.extension.github.info.impl;
 
 
@@ -15,6 +32,9 @@ import java.util.Map;
 import java.util.Objects;
 import java.util.Properties;
 
+/**
+ * @author RiverSharks
+ */
 public class PropertiesFilePluginInfoProvider implements PluginInfoProvider {
     private static final String BINARY_DATA_KEY = "binaryData";
     private static final String DESCRIPTION_KEY = "description";
